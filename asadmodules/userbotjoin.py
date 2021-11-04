b2e30a8c8bbc441f8294270ec44c9b8641b46f5c
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2021-11-04 13:04:45 +0500

    Update userbotjoin.py

--- asadmodules/userbotjoin.py
+++ asadmodules/userbotjoin.py
@@ -14,7 +14,7 @@ from pyrogram.errors import UserAlreadyParticipant
 
 
 @Client.on_message(
-    command(["play", f"play@{BOT_USERNAME}"]) & ~filters.private & ~filters.bot
+    command(["join", f"join@{BOT_USERNAME}"]) & ~filters.private & ~filters.bot
 )
 @authorized_users_only
 @errors
