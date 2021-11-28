ba9ce0857d29ad9b784061e0cfcf82c1e9871a81
Dr Asad Ali <91006488+jankarikiduniya@users.noreply.github.com>
2021-11-28 21:48:26 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -3,19 +3,24 @@
 # Rocks © @Dr_Asad_Ali © Rocks
 # Owner Asad + Harshit
 
+
+
+
 import asyncio
 from pytgcalls import idle
 from rocksdriver.asad import call_py, bot
 
 async def mulai_bot():
-    print("[INFO]: STARTING BOT CLIENT")
+    print("[ASAD]: STARTING BOT CLIENT")
     await bot.start()
-    print("[INFO]: STARTING PYTGCALLS CLIENT")
+    print("[ASAD]: STARTING PYTGCALLS CLIENT")
     await call_py.start()
     await idle()
     await pidle()
-    print("[INFO]: STOPPING BOT & USERBOT")
+    print("[ASAD]: STOPPING BOT & USERBOT")
     await bot.stop()
+
+# Roses are red, Violets are blue, A face like yours, Belongs in a zoo.
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(mulai_bot())
