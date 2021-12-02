5e55824aa68beeaee047e412b06c08eec5eabd52
Prog46-Amr187 <95185682+Prog46-Amr187@users.noreply.github.com>
2021-12-02 19:40:29 +0200

    Update downloader.py

--- asadmodules/downloader.py
+++ asadmodules/downloader.py
@@ -64,7 +64,7 @@ def song(_, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"**🎧 يتم الرفع [Java](https://t.me/JAI6H)**"
+        rep = f"**🎧 الرفع بواسطه: [Java](https://t.me/JAI6H)**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(float(dur_arr[i])) * secmul
